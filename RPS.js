3ee4b1b247b2f48827f1104c1794385206fd055e
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-05-04 16:03:49 +0200

    JS for computer choice

--- RPS.js
+++ RPS.js
@@ -1,1 +1,17 @@
-console.log("Hello")
+function getComputerChoice() {
+    let nrand = Math.floor(Math.random()*3); //gives a random number from 0 to 2 
+    //console.log(nrand);
+    if (nrand == 0) {
+        return "Rock";
+    }
+    if (nrand == 1) {
+        return "Paper";
+    }
+    if (nrand == 2) {
+        return "Scissor";
+    }
+    //if statements to convert the random number in a choice
+}
+
+let CChoice = getComputerChoice()
+console.log(CChoice)
